7b5f6936e9d96c961cb460d4eddca4e699c7d04f
miniDevel <wjdgus003@naver.com>
2023-07-14 15:52:25 +0900

    commit 2

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,12 +5,72 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextStyle customTextStyle =
+        TextStyle(fontWeight: FontWeight.w600, fontSize: 22);
+
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: [Image.asset('assets/img/home_screen.png')],
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "에서",
+                        style: customTextStyle,
+                      ),
+                      Text(
+                        "까지",
+                        style: customTextStyle,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "개 숫자 생성!",
+                        style: customTextStyle,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "중복 숫자 허용",
+                        style: customTextStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            BottomPicture(),
+            SizedBox(height: 100),
+          ],
         ),
       ),
     );
+  }
+}
+
+class BottomPicture extends StatelessWidget {
+  const BottomPicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Image.asset('assets/img/home_screen.png'),
+      Positioned(
+        top: 50,
+        right: 50,
+        child: Text(
+          '위를 입력하면 내가 숫자를 말해줄게!',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+        ),
+      ),
+    ]);
   }
 }
