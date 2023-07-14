ffef8cab8717125b1387eb69968b0f2b588140b2
miniDevel <wjdgus003@naver.com>
2023-07-14 11:28:59 +0900

    commit 1

---
+++ android/app/src/main/kotlin/com/example/generating_random_number/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.generating_random_number
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:generating_random_number/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'HancomMalangMalang'),
+    home: HomeScreen(),
+  ));
+}
+
